3cecab8b3ea21be772b6a60023cfd3ae78259de1
szym <szym17@g.com>
2017-11-29 15:23:22 +0100

    router events - dodanie napisu loading w prawym gornym rogu

--- tv-maniac/src/app/app.component.ts
+++ tv-maniac/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ResolveStart, ResolveEnd, NavigationError } from '@angular/router/';
+
 
 @Component({
   selector: 'tm-root',
@@ -7,10 +10,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sm';
+  isLoading = false;
 
-  clickHandler(event: MouseEvent) {
-    console.log('CLICK!');
-    this.title = this.title + '...' + event.clientX;
+  // clickHandler(event: MouseEvent) {
+  //   console.log('CLICK!');
+  //   this.title = this.title + '...' + event.clientX;
+  // }
+
+  constructor(router: Router) {
+      // router.events.subscribe(c => console.log(c));
+    router.events.subscribe(event => {
+      if (event instanceof ResolveStart) {
+        this.isLoading = true;
+      }
+      // tslint:disable-next-line:one-line
+      else if (event instanceof ResolveEnd || event instanceof NavigationError) {
+        this.isLoading = false;
+      }
+    });
   }
 }
 
