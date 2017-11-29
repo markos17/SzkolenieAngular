ac6c69d60752dee7c06b9c3142a9cdd8d228f3e6
szym <szym17@g.com>
2017-11-29 11:53:35 +0100

    json server

--- tv-maniac/src/app/bookmarks/bookmarks.service.ts
+++ tv-maniac/src/app/bookmarks/bookmarks.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Bookmark, BookmarkId } from './bookmarks.models';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class BookmarksService {
   private items: Bookmark[] = [];
 
-  constructor() {}
+  private readonly baseUrl = 'http://localhost:3000/bookmarks';
+
+  constructor(private http: HttpClient) {
+    this.http.get<Bookmark[]>(this.baseUrl)
+    .subscribe(items => this.items = items);
+  }
 
   getAll(): Bookmark[] {
     return [...this.items];
@@ -13,11 +19,15 @@ export class BookmarksService {
 
   add(item: Bookmark): void {
     // this.items.push(item);
-   this.items = [... this.items, item];
+    this.http.post(this.baseUrl, item)
+    .subscribe(() => this.items = [... this.items, item]);
+   // this.items = [... this.items, item];
   }
 
   remove(id: BookmarkId): void {
-    this.items = this.items.filter(item => item.id !== id);
+    this.http.delete(`${this.baseUrl}/${id}`)
+    .subscribe(() => this.items = this.items.filter(item => item.id !== id));
+  //  this.items = this.items.filter(item => item.id !== id);
   }
 
   has(id: BookmarkId): boolean {
