f1353403508d3a97f61a3ed066156bd49e617f3f
szym <szym17@g.com>
2017-11-29 10:55:54 +0100

    nowe

---
+++ tv-maniac/src/app/bookmarks/bookmark-add/bookmark-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tm-bookmark-add',
+  templateUrl: './bookmark-add.component.html',
+  styleUrls: ['./bookmark-add.component.scss']
+})
+export class BookmarkAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- tv-maniac/src/app/bookmarks/bookmarks.module.ts
+++ tv-maniac/src/app/bookmarks/bookmarks.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookmarksService } from './bookmarks.service';
+import { BookmarkAddComponent } from './bookmark-add/bookmark-add.component';
 
 @NgModule({
   imports: [
@@ -9,6 +10,6 @@ import { BookmarksService } from './bookmarks.service';
   providers: [
     BookmarksService
   ],
-  declarations: []
+  declarations: [BookmarkAddComponent]
 })
 export class BookmarksModule { }
